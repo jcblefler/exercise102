7dd20b1dfa1989e1acf54f0a36d890873332b2d0
GBTC408005UR <GBTC408005UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-03-11 16:28:01 -0400

    This is exercise 1.02

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String homePage(Model model){
+        model.addAttribute("myvarOne", "Say hello to the left.");
+        model.addAttribute("myvarTwo", "Say hello to the right.");
+        return "hometemplate";
+    }
+}
